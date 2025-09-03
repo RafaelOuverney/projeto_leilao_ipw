a7bcc140b230b7e3fd32f48e012a12b079bce4b8
Rafael Ouverney <rafael_ouverney31@hotmail.com>
2025-09-03 15:16:08 -0300

    Projeto Leilão IPW

--- backend/backend/src/main/java/io/github/rafaelouverney/backend/dto/PessoaRequisicaoDTO.java
+++
@@ -1,9 +0,0 @@
-package io.github.rafaelouverney.backend.dto;
-
-import lombok.Data;
-
-@Data
-public class PessoaRequisicaoDTO {
-    private String email;
-    private String senha;
-}
--- backend/backend/src/main/java/io/github/rafaelouverney/backend/model/Status.java
+++
@@ -1,4 +0,0 @@
-package io.github.rafaelouverney.backend.model;
-
-public class Status {
-}
